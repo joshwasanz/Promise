ea59f341d303ab4973500cc50861709ad72247f0
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-04 08:15:31 -0500

    edit fucntionality added for rooms

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("",view=views.home,name="home"),
     path("room/<str:pk>/",view=views.room,name="room"),
-    path("create-room/",view=views.createRoom,name="create-room")
+    path("create-room/",view=views.createRoom,name="create-room"),
+    path("update-room/<str:pk>",view=views.updateRoom,name="update-room")
 ]
