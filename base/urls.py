c57c165470ffbb9ec0cf0b7fe24cb130abbeae1b
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-05 05:42:24 -0500

    login setup

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
+
+    path("login/",views.loginPage,name='login'),
+
     path("",view=views.home,name="home"),
     path("room/<str:pk>/",view=views.room,name="room"),
     path("create-room/",view=views.createRoom,name="create-room"),
